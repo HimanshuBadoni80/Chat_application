4bb948871fe2f039e6a977e79942e2b0d80ea645
Himanshu Badoni <himanshubadoni80@gmail.com>
2026-02-04 05:57:39 +0000

    create lib/auth.ts(getsession) dashboard/layout.ts proxy.ts

--- lib/mail/mail.ts
+++ lib/mail/mail.ts
@@ -1,3 +1,4 @@
+// this file is specific to resend
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
---
+++ proxy.ts
@@ -0,0 +1,46 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function proxy(request: NextRequest) {
+  const sessionToken = request.cookies.get("session_token")?.value;
+  const { pathname } = request.nextUrl;
+  const isAuthPage = pathname === "/login" || pathname === "/signup";
+  //   if (!sessionToken) {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   } // The Web Response.redirect() API requires a URL object — not a string.
+
+  /* At the HTTP level, a redirect is just:
+    HTTP/1.1 307 Temporary Redirect
+    Location: /login
+ */
+
+  // better
+  // If trying to access protected area WITHOUT a session
+  if (!isAuthPage && !sessionToken) {
+    const loginUrl = new URL("/login", request.url); // always use absolute path "/" not relative path "login".
+
+    loginUrl.searchParams.set(
+      "from",
+      request.nextUrl.pathname + request.nextUrl.search,
+    );
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // If trying to access login while ALREADY logged in
+  if (isAuthPage && sessionToken) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    // protected routes
+    "/dashboard/:path*",
+    "/profile/:path*",
+    //Auth Areas (to redirect users who are already logged in)
+    "/login",
+    "/signup",
+  ],
+};
